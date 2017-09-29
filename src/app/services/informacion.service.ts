ca1ed3dafcc0c6da4aaf0c7b1f7641c35c2e917c
JohanZaHe <jezapata506@misena.edu.co>
2017-09-29 18:22:55 -0500

    Servicio de firebase listo

--- src/app/services/informacion.service.ts
+++ src/app/services/informacion.service.ts
@@ -5,16 +5,31 @@ import {Http} from '@angular/http';
 export class InformacionService {
   
   info:any = {};
+  equipo:any [] = [];
   cargada:boolean = false;
+  cargada_sobre_nosotros: boolean = false;
+
   constructor(public http:Http) { 
 
+   this.cargar_info();
+   this.cargar_sobre_nosotros();
+
+  }
+
+  public cargar_info(){
     this.http.get('assets/data/info.pagina.json')
-    .subscribe(data => {
-      console.log(data.json());
+    .subscribe(data => { 
       this.info = data.json();
       this.cargada = true;
     })
-
   }
+
+ public  cargar_sobre_nosotros(){
+    this.http.get('https://portafolio-b551f.firebaseio.com/equipo.json').subscribe(data => {
+      console.log(data.json());
+      this.equipo=data.json();
+      this.cargada_sobre_nosotros=true;
+    })
+ }
    
 }

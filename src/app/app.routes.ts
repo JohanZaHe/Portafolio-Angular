efbc245169cdbc3c5d05975fad101680474e674a
JohanZaHe <jezapata506@misena.edu.co>
2017-09-29 11:48:48 -0500

    Rutas agregadas

---
+++ src/app/app.routes.ts
@@ -0,0 +1,15 @@
+import { RouterModule, Routes } from '@angular/router';
+import {
+    AboutComponent,
+    PortafolioComponent,
+    ItemPortafolioComponent
+ } from './components/index.paginas';
+
+const app_routes: Routes = [
+    { path: '', component: PortafolioComponent },
+    { path: 'about', component: AboutComponent },
+    { path: 'item', component: ItemPortafolioComponent },
+    { path: '**', pathMatch: 'full', redirectTo: '' }
+];
+
+export const app_routing = RouterModule.forRoot(app_routes, {useHash:true});

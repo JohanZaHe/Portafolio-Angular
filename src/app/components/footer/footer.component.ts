a322f9589981d7839c61b3488473450adb40ce56
JohanZaHe <jezapata506@misena.edu.co>
2017-09-29 13:42:36 -0500

    Consumiendo servicio info sitio

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-
+import { InformacionService } from '../../services/informacion.service';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html'
 })
 export class FooterComponent {
    anio: number; 
-  constructor() { 
+  constructor(public _is:InformacionService) { 
     this.anio=new Date().getFullYear();
   }
 

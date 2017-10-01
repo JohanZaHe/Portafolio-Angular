6484b0ae67db3ac9d3c5a62d2e0e7eb09c3a8931
JohanZaHe <jezapata506@misena.edu.co>
2017-10-01 11:09:43 -0500

    "Módulo de busqueda terminado"

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -4,31 +4,75 @@ import { Http } from '@angular/http';
 @Injectable()
 export class ProductosService {
 
-  products:any[]=[];
-  cargando:boolean=true;
+  products: any[] = [];
+  productos_filtrado: any[] = [];
+  cargando: boolean = true;
+  filtrando: boolean = true;
 
-  constructor(public http:Http) { 
+  constructor(public http: Http) {
 
-      this.cargar_productos();
+    this.cargar_productos();
 
   }
 
-  public cargar_productos(){
-    this.cargando=true;
-   if(this.products.length==0){
-      this.http.get('https://portafolio-b551f.firebaseio.com/productos_idx.json')
-      .subscribe(res => {
-        setTimeout(()=>{
-          this.products=res.json();
-          this.cargando=false;
-        },1500)
+  public cargar_productos() {
+    this.cargando = true;
+    let promesa = new Promise((resolve, reject) => {
+      if (this.products.length == 0) {
+        this.http.get('https://portafolio-b551f.firebaseio.com/productos_idx.json')
+          .subscribe(res => {
+            setTimeout(() => {
+              this.products = res.json();
+              this.cargando = false;
+              resolve();
+            }, 1500)
+          });
+      }
+    })
+    return promesa;
+  }
+
+  public cargar_producto(id: string) {
+    return this.http.get('https://portafolio-b551f.firebaseio.com/productos/' + id + '.json')
+  }
+
+  public buscar_producto(termino: string) {
+    
+    this.filtrando=true;
+
+    //console.log('Buscando Productos');
+    //console.log(this.products.length);
+
+    if (this.products.length === 0) {
+
+      this.cargar_productos().then(() => {
+        //Terminó la carga
+        this.filtrar_productos(termino);
       });
+
+    } else {
+
+      this.filtrar_productos(termino);
+
     }
 
   }
 
-  public cargar_producto(id:string){
-    return this.http.get('https://portafolio-b551f.firebaseio.com/productos/'+id+'.json')
+  public filtrar_productos(termino: string) {
+    
+    this.productos_filtrado = [];
+
+    termino =  termino.toLocaleLowerCase();
+
+    this.products.forEach(prod => {
+      if (prod.categoria.toLocaleLowerCase().indexOf(termino) >= 0 || prod.titulo.toLocaleLowerCase().indexOf(termino) >= 0) {
+        this.productos_filtrado.push(prod);
+        //console.log(prod);
+      }
+      //console.log(prod);
+    })
+
+    this.filtrando=false;
   }
 
 }
